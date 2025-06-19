86c3c1d29d0934abc0a9cf33fbbdd7c4449b1394
saltandpurple <d0ghouse@freenet.de>
2025-06-19 15:09:38 +0200

    added secrets for mlflow tracking server: both in argocd (external secrets) and in terraform (ssm params)

--- terraform/secrets/main.tf
+++ terraform/secrets/main.tf
@@ -39,7 +39,9 @@ resource "terraform_data" "encrypt_secrets" {
     EOT
   }
   # just to ensure it doesn't overwrite stuff
-  depends_on = [aws_ssm_parameter.mlflow-postgres]
+  depends_on = [
+    aws_ssm_parameter.mlflow-postgres,
+  ]
 }
 
 
@@ -57,4 +59,45 @@ resource "aws_ssm_parameter" "mlflow-postgres" {
   depends_on = [aws_kms_alias.secrets]
 }
 
+resource "aws_ssm_parameter" "mlflow_admin_pw" {
+  name        = "/mllab/mlflow/admin-pw"
+  description = "MLflow admin password"
+  type        = "SecureString"
+  value       = var.mlflow-admin-pw
+  key_id      = aws_kms_key.secret_encryption.key_id
+
+  tags = {
+    Environment = "mllab"
+  }
+
+  depends_on = [aws_kms_alias.secrets]
+}
+
+resource "aws_ssm_parameter" "mlflow_admin_user" {
+  name        = "/mllab/mlflow/admin-user"
+  description = "MLflow admin username"
+  type        = "SecureString"
+  value       = var.mlflow-admin-user
+  key_id      = aws_kms_key.secret_encryption.key_id
+
+  tags = {
+    Environment = "mllab"
+  }
+
+  depends_on = [aws_kms_alias.secrets]
+}
+
+resource "aws_ssm_parameter" "mlflow_flask_server_secret_key" {
+  name        = "/mllab/mlflow/flask-server-secret-key"
+  description = "MLflow Flask server secret key"
+  type        = "SecureString"
+  value       = var.mlflow-flask-server-secret-key
+  key_id      = aws_kms_key.secret_encryption.key_id
+
+  tags = {
+    Environment = "mllab"
+  }
+
+  depends_on = [aws_kms_alias.secrets]
+}
 
--- terraform/secrets/variables.tf
+++ terraform/secrets/variables.tf
@@ -3,3 +3,24 @@ variable "mlflow-postgres-pw" {
   sensitive = true
   default = "" # required, so the tf plan doesn't fail
 }
+
+variable "mlflow-admin-pw" {
+  type = string
+  sensitive = true
+  default = "" # required, so the tf plan doesn't fail
+  description = "Password for MLflow admin user"
+}
+
+variable "mlflow-admin-user" {
+  type = string
+  sensitive = true
+  default = "admin" # default admin username
+  description = "Username for MLflow admin access"
+}
+
+variable "mlflow-flask-server-secret-key" {
+  type = string
+  sensitive = true
+  default = "" # required, so the tf plan doesn't fail
+  description = "Secret key for Flask server in MLflow"
+}
